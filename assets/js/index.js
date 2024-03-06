6c1619515caa4c8028cf9aaf949d192e310d398b
MiracleChibuike <miraclechibuike27@gmail.com>
2024-03-06 22:11:16 +0100

    verified

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const showMenu = document.getElementById("showMenu");
 const hideMenu = document.getElementById("hideMenu");
 var navbar = document.querySelector(".navbar");
+var parent = document.querySelector(".homeText");
+var hero = document.getElementById("hero");
 
 // Show the nav-contents on click
 
@@ -10,6 +12,16 @@ showMenu.addEventListener("click", ()=>{
         hideMenu.style.display = "block";
         showMenu.style.display = "none"
     }
+    
+   setTimeout(function () {
+        
+    })
+
+    var show_menuAudio = new Audio(
+      "assets/sounds/mixkit-correct-answer-tone-2870.wav"
+    );
+    show_menuAudio.volume = 0.3;
+    show_menuAudio.play();
 });
 
 // Hide the Nav-contents on icon close click
@@ -19,4 +31,15 @@ hideMenu.addEventListener("click", ()=>{
          hideMenu.style.display = "none";
          showMenu.style.display = "block";
     }
+    var hide_menuAudio = new Audio(
+      "assets/sounds/mixkit-confirmation-tone-2867.wav"
+    );
+    hide_menuAudio.volume = 0.3;
+    hide_menuAudio.play();
 })
+
+// const videoplay = document.getElementById("myVid");
+// var playbtn = document.getElementById("playBtn");
+// playbtn.addEventListener("click", ()=>{
+//     videoplay.play();
+// })
