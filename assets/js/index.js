c9f7e4283d35afea2839984ef545310eddfa3c26
MiracleChibuike <miraclechibuike27@gmail.com>
2024-03-07 14:31:00 +0100

    done-products

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,8 +38,58 @@ hideMenu.addEventListener("click", ()=>{
     hide_menuAudio.play();
 })
 
-// const videoplay = document.getElementById("myVid");
-// var playbtn = document.getElementById("playBtn");
-// playbtn.addEventListener("click", ()=>{
-//     videoplay.play();
-// })
+// https://youtu.be/Z2MyXuHstIs?si=U_pxFug9zraRTgYo
+
+// to play our videos
+const laptop_videos = document.getElementById("playBtn");
+laptop_videos.addEventListener("click", ()=>{
+    alert("you are being re-directed to our official YouTube Channel where you can watch all of our videos")
+    window.location.href = "https://youtu.be/Z2MyXuHstIs?si=U_pxFug9zraRTgYo";
+})
+
+// show more action
+var show_moreBtn = document.getElementById("show-More-Btn");
+var show_lessBtn = document.getElementById("show-Less-Btn");
+var show_MoreDiv = document.querySelector(".showMoreDiv");
+
+show_moreBtn.addEventListener("click", ()=>{
+    if (show_MoreDiv.style.display = "none") {
+        show_MoreDiv.style.display ="block";
+        show_MoreDiv.style.transition = "all .5s ease"
+    }
+    show_moreBtn.style.display = "none"
+});
+
+show_lessBtn.addEventListener("click", ()=>{
+    if (show_MoreDiv.style.display = "block") {
+        show_MoreDiv.style.display = "none"
+    };
+    show_moreBtn.style.display = "block";
+})
+
+var showdiv2 = document.querySelector(".showdiv2");
+var btn2 = document.querySelector(".showBtn2");
+var btn2_less = document.querySelector(".lessbtn2");
+
+btn2.addEventListener("click", ()=>{
+    showdiv2.style.display = "block";
+    btn2.style.display = "none"
+});
+
+btn2_less.addEventListener("click", ()=>{
+    showdiv2.style.display = "none";
+    btn2.style.display = "block"
+});
+
+var btn3_Show = document.querySelector(".showBtn3");
+var showDiv3 = document.querySelector(".moreDiv3");
+var btn3_less = document.querySelector(".less3");
+btn3_Show.addEventListener("click", () =>{
+    showDiv3.style.display = "block";
+    btn3_Show.style.display = "none"
+});
+
+btn3_less.addEventListener("click", () => {
+  showDiv3.style.display = "none";
+  btn3_Show.style.display = "block";
+});
